50520128beabd36df127c01c9309ab8f2fa9f98c
catagoyanarte <cata.goyanarte@icloud.com>
2024-11-10 22:39:24 -0300

    modifique un par de cosas en los formularios, como tipografia etc

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-let api = "https://dummyjson.com/recipies?limit=10"
+let api = "https://dummyjson.com/recipes?limit=10"
 
 fetch(api)
-.then(function(response){
+  .then(function (response) {
     return response.json();
-})
-.then(function (data) {
-   console.log(data);
+  })
+  .then(function (data) {
+    console.log(data);
 
 
-   
-})
+    let recetas = data.recipes
+
+    console.log(recetas)
+
+    let contenedorRecetas = document.querySelector(".recetas")
+
+    recetas.forEach(function (receta) {
+        console.log(receta.name);
+
+        contenedorRecetas.innerHTML += `
+        <div>
+          <div>
+            <h4>${receta.name}</h4>
+            <p>Cocina: ${receta.cuisine}</p>
+            <span>${receta.difficulty}</span>
+            <a href="receta.html?id=${receta.id}">Ver receta</a>
+          </div>
+        </div>`
+});
+});
