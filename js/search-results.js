07a161285ea1c79d4f98cf2c6eb158261dd51f6d
catagoyanarte <cata.goyanarte@icloud.com>
2024-11-15 12:22:28 -0300

    agregue un par de cosas de results

--- js/search-results.js
+++ js/search-results.js
@@ -1,1 +1,10 @@
-
+let queryString = location.search
+let obj = NEW URLSearchParams(queryString)
+let palabraBuscada = obj.get("search")
+console.log(palabraBuscada)
+let result = document.querySelector(".result")
+if (palabraBuscada == ''){
+    result.innerText = "El campo esta vacio"
+}else if (palabraBuscada.length < 3){
+    result.innerText= 'el campo debe ser mayor a 3'
+}
