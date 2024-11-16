031575d0f6b4a3c13b997ef53134c2ba0f64a804
manuelajudez <manuelajudezrivas@yahoo.com>
2024-11-16 13:04:52 -0300

    aplico // frente al js del searchbar para continuar poder continuar sin que se ejecute un error

--- js/login.js
+++ js/login.js
@@ -1,23 +1,23 @@
-let form= document.querySelector("formulario");
-let searchbar = document.querySelector("#searchbar");
-let errorMensaje = document.querySelector("#error-message");
-form.addEventListener("submit", function (e) {
-	e.preventDefault();
-    errorMensaje.style.display= "none";
-    errorMensaje.innerText= "";
-	let valida= true
-if (searchbar==="") {
-    errorMensaje.style.display ="block" ; 
-    errorMensaje.innerText ="el campo esta vacio";  
-    valida=false
-}
-if (searchbar.length< 3) {
-    errorMensaje.style.display="block";
-    errorMensaje.innerText="el campo no puede ser menor a 8 numeros "
-    valida=false
-
-}
-if(valida){
-    form.submit();
-}
-})
+// let form= document.querySelector("formulario");
+// let searchbar = document.querySelector("#searchbar");
+// let errorMensaje = document.querySelector("#error-message");
+// form.addEventListener("submit", function (e) {
+// 	e.preventDefault();
+//     errorMensaje.style.display= "none";
+//     errorMensaje.innerText= "";
+// 	let valida= true
+// if (searchbar==="") {
+//     errorMensaje.style.display ="block" ; 
+//     errorMensaje.innerText ="el campo esta vacio";  
+//     valida=false
+// }
+// if (searchbar.length< 3) {
+//     errorMensaje.style.display="block";
+//     errorMensaje.innerText="el campo no puede ser menor a 8 numeros "
+//     valida=false
+// 
+// }
+// if(valida){
+//     form.submit();
+// }
+// })
