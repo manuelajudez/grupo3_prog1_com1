a8d4dcdf3743e304684b0f8b321cdc5d456d44be
manuelajudez <manuelajudezrivas@yahoo.com>
2024-11-16 12:33:54 -0300

    acepto cambios de merge

--- js/search-results.js
+++ js/search-results.js
@@ -4,7 +4,7 @@ let palabraBuscada = obj.get("search")
 console.log(palabraBuscada)
 let result = document.querySelector(".result")
 if (palabraBuscada == ''){
-    result.innerText = "El campo esta vacio"
+    result.innerText = "El campo esta vacio";
 }else if (palabraBuscada.length < 3){
-    result.innerText= 'el campo debe ser mayor a 3'
+    result.innerText= 'el campo debe ser mayor a 3';
 }
