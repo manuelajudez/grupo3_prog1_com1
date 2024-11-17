741a998226f9893278d1936dd6b771e568ad14db
Onabeitia <onabeitia@icloud.com>
2024-11-16 21:20:33 -0300

    js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,30 @@
-let formulario = document.querySelector("#cuestionario"); 
-let searchbar = document.querySelector("#searchbar"); 
-let errorMensaje = document.querySelector("#error-mensaje"); 
-
-formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    errorMensaje.style.display = "none"; 
-    errorMensaje.innerText = ""; 
-
-    let valida = true; 
-
-    if (searchbar.value === "") {
-        errorMensaje.style.display = "block";
-        errorMensaje.innerText = "El campo está vacío";
-        valida = false;
-    }
-    
-    else if (searchbar.value.length < 3) {
-        errorMensaje.style.display = "block";
-        errorMensaje.innerText = "El campo debe tener al menos 3 caracteres";
-        valida = false;
-    }
-    
-    if (valida) {
-        formulario.submit();
-    }
-});
+//let formulario = document.querySelector("#cuestionario"); 
+//let searchbar = document.querySelector("#searchbar"); 
+//let errorMensaje = document.querySelector("#error-mensaje"); 
+//
+//formulario.addEventListener("submit", function (e) {
+//    e.preventDefault();
+//    errorMensaje.style.display = "none"; 
+//    errorMensaje.innerText = ""; 
+//
+//   let valida = true; 
+//
+//    if (searchbar.value === "") {
+//        errorMensaje.style.display = "block";
+//        errorMensaje.innerText = "El campo está vacío";
+//        valida = false;
+//    }
+//   
+//    else if (searchbar.value.length < 3) {
+//        errorMensaje.style.display = "block";
+//        errorMensaje.innerText = "El campo debe tener al menos 3 caracteres";
+//        valida = false;
+//    }
+//    
+//    if (valida) {
+//        formulario.submit();
+//    }
+//});
 
 let log_form = document.querySelector(".login_form");
 let log_mail = document.querySelector("#log_email");
--- js/register.js
+++ js/register.js
@@ -1,31 +1,3 @@
-let formulario = document.querySelector("#cuestionario"); 
-let searchbar = document.querySelector("#searchbar"); 
-let errorMensaje = document.querySelector("#error-mensaje"); 
-
-formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    errorMensaje.style.display = "none"; 
-    errorMensaje.innerText = ""; 
-
-    let valida = true; 
-
-    if (searchbar.value === "") {
-        errorMensaje.style.display = "block";
-        errorMensaje.innerText = "El campo está vacío";
-        valida = false;
-    }
-    
-    else if (searchbar.value.length < 3) {
-        errorMensaje.style.display = "block";
-        errorMensaje.innerText = "El campo debe tener al menos 3 caracteres";
-        valida = false;
-    }
-    
-    if (valida) {
-        formulario.submit();
-    }
-});
-
 let reg_form = document.querySelector(".register_form");
 let mail = document.querySelector("#email");
 let contrasena = document.querySelector("#contrasena");
