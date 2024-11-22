9ed9c0068bc9d409030e118640326ad1e7f87872
Onabeitia <onabeitia@icloud.com>
2024-11-22 16:26:55 -0300

    hice el js de search results

--- js/search-results.js
+++ js/search-results.js
@@ -1,35 +1,42 @@
-let queryString = window.location.search; 
-let obj = new URLSearchParams(queryString); 
+let queryString = window.location.search;
+let obj = new URLSearchParams(queryString);
+let recetaBuscada = obj.get("search");
 
-let recetaBuscada = obj.get("search"); 
+let url = `https://dummyjson.com/recipes/search?q=${recetaBuscada}`;
+let contenedorRecetas = document.querySelector(".recetas");
+let tituloBusqueda = document.getElementById("titulo-busqueda");
 
-let url = `https://dummyjson.com/recipes/search?q=${recetaBuscada}`; 
+tituloBusqueda.textContent = `Resultados de búsqueda para: "${recetaBuscada}"`;
 
 fetch(url)
-.then(function(response) {
+  .then(function (response) {
     return response.json(); 
-})
+  })
+  .then(function (data) {
+    console.log(data, `data`);
+    let recetas = data.recipes;
+    if (recetas.length > 0) {
 
-.then(function(data) {
-    console.log(data, `data`)
-
-    let recetas = data.recipes
-
-    let contenedorRecetas = document.querySelector(".recetas")
-    recetas.forEach(function (receta) {
-        console.log(receta);
-        contenedorRecetas.innerHTML += `
-         <article class="contenedor_recetas">
+        for (let i = 0; i < recetas.length; i++) {
+          let receta = recetas[i];
+          let recetaHTML = `
+            <article class="contenedor_recetas">
               <h4 class="recetas_name">${receta.name}</h4>
               <p class="recetas_cuisine">${receta.cuisine}</p>
-              <img src="${receta.image}" alt="" class="receta_image">
-              <p class="recetas_difficulty">${receta.difficulty}</p>
+              <img src="${receta.image}" alt="${receta.name}" class="receta_image">
+              <p class="recetas_difficulty">Dificultad: ${receta.difficulty}</p>
               <a href="receta.html?id=${receta.id}" class="receta_id">Ver receta</a>
-            </article>`;
-});
-})
+            </article>
+          `;
+          contenedorRecetas.innerHTML += recetaHTML;
+        }
+      } else {
+        
+        contenedorRecetas.innerHTML = `<p>No hay coincidencias para tu búsqueda.</p>`;
+      }
+    })
+    .catch(function (err) {
 
-.catch(function(err) {
-    contenedorRecetas.innerHTML += `error al cargar lo que estabas buscando`;
-    console.log(err);
-});
+      contenedorRecetas.innerHTML = `<p>Error al cargar los resultados.</p>`;
+      console.error(err);
+    });
